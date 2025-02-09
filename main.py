a971407c0ef52ed6803868137a5a9e2c629c50b1
thanglm2006 <sungang.20072009@gmail.com>
2025-02-09 22:21:23 +0700

    GDPSO

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import math
 import numpy as np
 from sklearn.metrics import normalized_mutual_info_score
 import networkx as nx
@@ -12,7 +13,9 @@ c1=1.4961
 c2=1.4961
 ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 # in this code, I count the nodes from 0 to n-1
-with open('test.txt') as f:
+dataset='test'
+fitness_function='NMI'
+with open(f'/home/thanglm2006/dataset/{dataset}.txt') as f:
     lines = f.readlines()
     tmp=lines[0].split()
     n = int(tmp[0])
@@ -33,10 +36,6 @@ with open('test.txt') as f:
 for i in range(n):
     for j in range(n):
       degree_array[i]+=adj[i][j]
-for i in range(n):
-    for j in range(n):
-        if(adj[i][j]!=adj[j][i]):
-            print("Error")
 GBest=np.arange(n)
 population_X=np.array([np.arange(n) for i in range(population)])
 PBest=np.copy(population_X)
@@ -68,9 +67,16 @@ def fitness_calculator2(tempCommunity,originCommunity,fitness,u):
     return tempFitness
 ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 def NMI_calculator(community):
-    G=nx.karate_club_graph()
-    true_labels=[1 if G.nodes[i]['club']=='Officer' else 0 for i in G.nodes()]
-    return normalized_mutual_info_score(community,true_labels)
+    if dataset=='karate':
+        G=nx.karate_club_graph()
+        true_labels = [1 if G.nodes[i]['club'] == 'Officer' else 0 for i in G.nodes()]
+        return normalized_mutual_info_score(community, true_labels)
+    elif dataset=='football':
+        G = nx.read_gml('/home/thanglm2006/football.gml')
+        true_labels = [G.nodes[node]['value'] for node in G.nodes]
+        return normalized_mutual_info_score(community, true_labels)
+    else:
+        return 0
 def reorder(tempCommunities):
     '''This is for avoiding the case that the community is the same as original community'''
     tmp=np.arange(n)
@@ -89,6 +95,62 @@ def reorder(tempCommunities):
             tempCommunities[j]=idx
         idx+=1
     return tempCommunities
+def update_status(i,j):
+    global GBest,Gbest_fitness
+    ''''''''''''''''''''''''''''''''''''''''pop_v updating'''''''''''''''''''''''''''''''''''''''''
+    r1 = np.random.uniform(0, 1)
+    r2 = np.random.uniform(0, 1)
+    population_V[j] = w * population_V[j]
+    v1 = (population_X[j] != PBest[j]).astype(int)
+    v2 = (GBest != population_X[j]).astype(int)
+    tmp1 = c1 * r1 * v1
+    tmp2 = c2 * r2 * v2
+    tmp3 = tmp1 + tmp2
+    population_V[j] = ((population_V[j] + tmp3) >= 1).astype(int)
+    '''''''''''''''''''''''''''''''''''''''' '''''''''''''''''''''''''''''''''''''''''
+
+    ''''''''''''''''''''''''''''''''''''''''updating pop_x'''''''''''''''''''''''''''''''''''''''''
+    for k in range(n):
+        if population_V[j][k] == 0:
+            continue
+        else:
+            best_finess = -999999.0
+            best_communities = np.copy(population_X[j])
+            for l in adj_list[k]:
+                tempCommunities = np.copy(population_X[j])
+                tempCommunities[k] = tempCommunities[l]
+                tempFitness = NMI_calculator(tempCommunities)
+                if tempFitness == 0:
+                    tempFitness = fitness_calculator2(tempCommunities, population_X[j], population_fitness[j], k)
+                if tempFitness > best_finess:
+                    best_finess = tempFitness
+                    best_communities = np.copy(tempCommunities)
+            best_communities = reorder(best_communities)
+            best_finess = NMI_calculator(best_communities)
+            if best_finess == 0:
+                best_finess = fitness_calculator2(best_communities, population_X[j], population_fitness[j], k)
+            population_X[j] = np.copy(best_communities)
+            population_fitness[j] = best_finess
+            if best_finess > PBest_fitness[j]:
+                PBest[j] = np.copy(best_communities)
+                PBest_fitness[j] = best_finess
+                if best_finess > Gbest_fitness:
+                    Gbest_fitness = best_finess
+                    GBest = np.copy(best_communities)
+                    if math.isclose(best_finess, 1.0, rel_tol=1e-9) and fitness_function == 'NMI':
+                        print("Iteration ", i, ": ", '\n', Gbest_fitness, " ", GBest)
+                        end_time = time.time()
+                        total_running_time = end_time - start_time
+                        print(
+                            f"Total running time: {total_running_time:.6f} seconds or {int(total_running_time / 60)} minutes and {total_running_time % 60:.6f} seconds")
+                        print("final result: ")
+                        print(f"best fitness with {fitness_function} formula: ", Gbest_fitness)
+                        print("number of communities: ", len(np.unique(GBest)))
+                        print("communities: ", GBest)
+                        exit()
+                    Gbest_fitness = best_finess
+                    GBest = np.copy(best_communities)
+    '''''''''''''''''''''''''''''''''''''''' '''''''''''''''''''''''''''''''''''''''''
 '''''''''''''''''''''''''''''''''''''''''''''''''''Main'''''''''''''''''''''''''''''''''''''''''''''''''''
 
 '''Population Initialization'''
@@ -97,7 +159,11 @@ for i in range(population):
         random_vertice = np.random.randint(0, n - 1)
         for k in adj_list[random_vertice]:
             population_X[i][k] = population_X[i][random_vertice]
-    population_fitness[i] = fitness_calculator(population_X[i])
+    population_fitness[i] = NMI_calculator(population_X[i])
+    if population_fitness[i]==0:
+        fitness_function = 'Q'
+        population_fitness[i]=fitness_calculator(population_X[i])
+
 
     check1=0
     check2=0
@@ -119,51 +185,12 @@ print("Initial result: ",Gbest_fitness," ",GBest)
 ''''''''''''''''''''''''''''''''''''''''Main Loop'''''''''''''''''''''''''''''''''''''''''
 for i in range(Max_Iterations):
     for j in range(population):
-        ''''''''''''''''''''''''''''''''''''''''pop_v updating'''''''''''''''''''''''''''''''''''''''''
-        r1=np.random.uniform(0,1)
-        r2=np.random.uniform(0,1)
-        population_V[j]=w*population_V[j]
-        v1=(population_X[j]!=PBest[j]).astype(int)
-        v2=(GBest!=population_X[j]).astype(int)
-        tmp1=c1*r1*v1
-        tmp2=c2*r2*v2
-        tmp3=tmp1+tmp2
-        population_V[j]=((population_V[j]+tmp3)>=1).astype(int)
-        '''''''''''''''''''''''''''''''''''''''' '''''''''''''''''''''''''''''''''''''''''
-
-        ''''''''''''''''''''''''''''''''''''''''updating pop_x'''''''''''''''''''''''''''''''''''''''''
-        for k in range(n):
-            if population_V[j][k]==0:
-                continue
-            else:
-                best_finess=-999999.0
-                best_communities=np.copy(population_X[j])
-                for l in adj_list[k]:
-                    tempCommunities=np.copy(population_X[j])
-                    tempCommunities[k]=tempCommunities[l]
-                    tempFitness=fitness_calculator2(tempCommunities,population_X[j],population_fitness[j],k)
-                    # tempFitness=fitness_calculator(tempCommunities)
-                    # print("tempFitness1: ",tempFitness1,'and',"tempFitness: ",tempFitness)
-                    if tempFitness>best_finess:
-                        # print("tempFitness: ",tempFitness,'\n',"PBest_Fitness: ",PBest_fitness[j])
-                        best_finess=tempFitness
-                        best_communities=np.copy(tempCommunities)
-                best_communities=reorder(best_communities)
-                best_finess=fitness_calculator2(best_communities,population_X[j],population_fitness[j],k)
-                population_X[j]=np.copy(best_communities)
-                population_fitness[j]=best_finess
-                if best_finess>PBest_fitness[j]:
-                    PBest[j]=np.copy(best_communities)
-                    PBest_fitness[j]=best_finess
-                    if best_finess>Gbest_fitness:
-                        Gbest_fitness=best_finess
-                        GBest=np.copy(best_communities)
-        '''''''''''''''''''''''''''''''''''''''' '''''''''''''''''''''''''''''''''''''''''
+        update_status(i,j)
     print("Iteration ",i,": ",'\n',Gbest_fitness," ",GBest)
 end_time=time.time()
 total_running_time=end_time-start_time
-print(f"Total running time: {total_running_time:.6f} seconds or {(total_running_time/60):.6f} minutes")
+print(f"Total running time: {total_running_time:.6f} seconds or {int(total_running_time / 60)} minutes and {total_running_time % 60} seconds")
 print("final result: ")
-print("best fitness: ",Gbest_fitness)
+print(f"best fitness with {fitness_function} formula: ",Gbest_fitness)
 print("number of communities: ",len(np.unique(GBest)))
 print("communities: ",GBest)
