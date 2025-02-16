49c1b991be26ba7c4eec3e966869e805b10ffd9c
thanglm2006 <sungang.20072009@gmail.com>
2025-02-16 20:17:25 +0700

    GDPSO

--- BenchMark..py
+++ BenchMark..py
@@ -14,13 +14,13 @@ community={frozenset(G.nodes[v]['community']) for v in G}
 print(community)
 numNode=G.number_of_nodes()
 numEdge=G.number_of_edges()
-with open("/home/thanglm2006/Experimental data/synthetic networks/GN/GN-1.00/network.dat", "w") as f:
+with open("Experimental data/synthetic networks/GN/GN-1.00/network.dat", "w") as f:
     writer = csv.writer(f,delimiter=" ")
     writer.writerow([numNode,numEdge])
     for u,v in G.edges():
         writer.writerow([u,v])
 node_community = {node: list(data["community"])[0] for node, data in G.nodes(data=True)}
-with open("/home/thanglm2006/Experimental data/synthetic networks/GN/GN-1.00/community.dat", "w") as f:
+with open("Experimental data/synthetic networks/GN/GN-1.00/community.dat", "w") as f:
     writer = csv.writer(f,delimiter=" ")
     for node, data in G.nodes(data=True):
         writer.writerow([node, list(data["community"])[0]])
